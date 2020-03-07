69abccd7424cb5e01291c2029326ea2384a29193
Georgie <georginakellis@gmail.com>
2020-03-07 16:40:08 +0800

    js-add-general-updates

--- questions.js
+++ questions.js
@@ -1,1 +1,27 @@
-
+var questions = [
+    {
+      title: "One loop inside the body of another loop is called ",
+      choices: ["Loop in loop", "Nested", "Double loops", "Two loops"],
+      answer: "Nested"
+    },
+    {
+      title: "Inside which HTML element do we put the JavaScript?",
+      choices: ["<javascript>", "<scriptor>", "<scripting>", "<script>"],
+      answer: "<script>"
+    },
+    {
+      title: "Where should you insert JavaScript?",
+      choices: ["the <head>", "the bottom of the <body>", "anywhere in the HTML", "anywhere in the stylesheet"],
+      answer: "the bottom of the <body>"
+    },
+    {
+      title: "How do you write 'Hello World' in an alert box?",
+      choices: ["msgBox('Hello World)", "alertBox('Hello World')", "alert('Hello World')", "msg('Hello World)"],
+      answer: "alert('Hello World')"
+    },
+    {
+      title: "How do you call a function called myFunction?",
+      choices: ["myFunction()", "call myFunction()", "call function myFunction", "call select myFunction"],
+      answer: "myFunction()"
+    },
+  ]
